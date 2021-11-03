a5cc43923f22d2d3ccd6695a1766e824c71f17c8
Vinicius Tirabassi de Oliveira <31051077+vtirabassi@users.noreply.github.com>
2021-11-03 09:25:37 -0300

    Update AppDbContext.cs

--- TodoApi/Data/AppDbContext.cs
+++ TodoApi/Data/AppDbContext.cs
@@ -1,13 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Model;
 
-namespace TodoApi.Data
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
+namespace TodoApi.Data;
 
-        public DbSet<Todo> Todos => Set<Todo>();
-    }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
 }
